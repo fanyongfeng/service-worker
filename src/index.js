fde00201157433d423ecb080dbec56b8b883ecc1
fanfan <fanfan@vipabc.com>
2018-02-02 09:42:19 +0800

    ppt

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js').then(function(registration) {
+        registration.update(); //可以一段时间之后触发
         console.log('ServiceWorker registration successful with scope: ',    registration.scope);
     }).catch(function(err) {
         console.log('ServiceWorker registration failed: ', err);
